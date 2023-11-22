0e44d5c5781eef3dd9a2ba091d21b6dcf2eca3cd
david <zhouyi021022@gmail.com>
2023-11-22 16:32:36 +1100

    upload

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    // Light weight controller
+    [ApiController]
+    [Route("api/home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
